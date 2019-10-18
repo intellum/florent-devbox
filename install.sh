c25073534518599487d790fba8c1a97cdd597415
Florent Guilleux <fguilleux@intellum.com>
2019-10-18 15:02:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 sudo apt-get update
 
-sudo install -y ghi
 sudo snap install hub --classic
-
 cd ~/exceed && gem install ghi
+sudo apt-get install -y neovim
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
 cp -RTs $PWD/home $HOME
