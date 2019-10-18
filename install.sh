3ba02e7fe508a66eec6757b19d38d2db66b1b19f
Florent Guilleux <fguilleux@intellum.com>
2019-10-18 15:08:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ cd ~/exceed && gem install ghi
 sudo apt-get install -y neovim
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp -RTs $PWD/home $HOME
